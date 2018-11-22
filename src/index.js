eb11e3e3740ea644564fe9e8888fe7fa33043e7a
Rohitt Vashishtha <aero31aero@gmail.com>
2018-11-23 02:31:18 +0530

    ui: Add inputs and buttons for image manipulation.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 img_utils.load_image();
 
-var encrypt = (x, n, r) => {
-	let boxes = sbox_factory({
-		x: 0.6,
-		n: 0.61,
-		r: 4,
-		count: 1000,
-	});
+var load_params = () => {
+	let opt = {};
+	opt.x = parseFloat(document.getElementById('X').value);
+	opt.n = parseFloat(document.getElementById('N').value);
+	opt.r = parseFloat(document.getElementById('R').value);
+	return opt;
+}
+
+var encrypt = () => {
+	let opt = load_params();
+	opt.count = 1000;
+	let boxes = sbox_factory(opt);
 	var x = img_utils.get_image_pixels('src-image');
 	x = img_utils.encrypt(x, boxes);
 	img_utils.paint_image_data(x, 'final-image');
 }
 
 var decrypt = () => {
-	let boxes = sbox_factory({
-		x: 0.6,
-		n: 0.61,
-		r: 4,
-		count: 1000,
-	});
+	let opt = load_params();
+	opt.count = 1000;
+	let boxes = sbox_factory(opt);
 	var x = img_utils.get_image_pixels('final-image');
 	x = img_utils.decrypt(x, boxes);
-	img_utils.paint_image_data(x, 'src-image');
+	// img_utils.paint_image_data(x, 'src-image');
 	img_utils.paint_image_data(x, 'reverse-image');
 }
-
 
 xx = encrypt;
 yy = decrypt;
